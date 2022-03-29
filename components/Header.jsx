aebd191eb0afff4312c9187b6904d615ed632f6f
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-03-29 23:28:47 +0200

    Make Header Sticky

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,32 @@
-import { useTheme } from "@emotion/react";
-const Header = ({ children }) => {
-  const { main } = useTheme()?.palette?.primary;
+import classes from "../styles/Header.module.scss";
+import { useEffect } from "react";
+import { useRef } from "react";
 
-  return <header style={{ backgroundColor: main }}>{children}</header>;
+const Header = ({ children }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const scrollFunction = () => {
+      if (
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
+      ) {
+        ref.current.style = "box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.75)";
+      } else {
+        ref.current.style.boxShadow = "none";
+      }
+    };
+
+    window.onscroll = () => {
+      scrollFunction();
+    };
+  });
+
+  return (
+    <header ref={ref} className={classes.header}>
+      {children}
+    </header>
+  );
 };
 
 export default Header;
