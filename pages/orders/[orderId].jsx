000c150ef66640778396d20f11f114a6f68c9811
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-03-30 22:28:20 +0200

    Add Order Page and Design

---
+++ pages/orders/[orderId].jsx
@@ -0,0 +1,162 @@
+import Image from "next/image";
+import Container from "@mui/material/Container";
+
+import myLoader from "../../helper/myLoader";
+import classes from "../../styles/Order.module.scss";
+
+const Order = () => {
+  const status = 0;
+
+  const statusClass = (index) => {
+    if (index - status < 1) return classes.done;
+    if (index - status === 1) return classes.inProgress;
+    if (index - status > 1) return classes.undone;
+  };
+  return (
+    <Container maxWidth="xl">
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <div className={classes.orderDetails}>
+            <div className={classes.table}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Order ID</th>
+                    <th>Customer</th>
+                    <th>Address</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className={classes.name}>12345678996325</td>
+                    <td className={classes.extra}>Khaled</td>
+                    <td className={classes.price}>Mansoura st. 215-112</td>
+                    <td className={classes.quantity}>$79.00</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className={classes.tracking}>
+            <div className={statusClass(0)}>
+              <div>
+                <Image
+                  alt="payment"
+                  loader={myLoader}
+                  src={"paid.png"}
+                  width={50}
+                  height={50}
+                  objectFit="content"
+                />
+              </div>
+              <p>Payment</p>
+              <div>
+                <Image
+                  alt="checked"
+                  loader={myLoader}
+                  src={"checked.png"}
+                  width={30}
+                  height={30}
+                  objectFit="content"
+                />
+              </div>
+            </div>
+            <div className={statusClass(1)}>
+              <div>
+                <Image
+                  alt="preparing"
+                  loader={myLoader}
+                  src={"bake.png"}
+                  width={50}
+                  height={50}
+                  objectFit="content"
+                />
+              </div>
+              <p>Preparing</p>
+              <div>
+                <Image
+                  alt="checked"
+                  loader={myLoader}
+                  src={"checked.png"}
+                  width={30}
+                  height={30}
+                  objectFit="content"
+                />
+              </div>
+            </div>
+            <div className={statusClass(2)}>
+              <div>
+                <Image
+                  alt="on the way"
+                  loader={myLoader}
+                  src={"bike.png"}
+                  width={50}
+                  height={50}
+                  objectFit="content"
+                />
+              </div>
+              <p>On the way</p>
+              <div>
+                <Image
+                  alt="checked"
+                  loader={myLoader}
+                  src={"checked.png"}
+                  width={30}
+                  height={30}
+                  objectFit="content"
+                />
+              </div>
+            </div>
+            <div className={statusClass(3)}>
+              <div>
+                <Image
+                  alt="delivered"
+                  loader={myLoader}
+                  src={"delivered.png"}
+                  width={50}
+                  height={50}
+                  objectFit="content"
+                />
+              </div>
+              <p>Delivered</p>
+              <div>
+                <Image
+                  alt="checked"
+                  loader={myLoader}
+                  src={"checked.png"}
+                  width={30}
+                  height={30}
+                  objectFit="content"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.right}>
+          <div className={classes.cartTotal}>
+            <h1>CART TOTAL</h1>
+            <div>
+              <p>
+                <span>Subtotal</span>
+                <span>$79.60</span>
+              </p>
+              <p>
+                <span>Discount:</span>
+                <span>$0.00</span>
+              </p>
+              <hr />
+              <p>
+                <span>Total:</span>
+                <span>$79.60</span>
+              </p>
+            </div>
+            <button disabled>PAID</button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Order;
